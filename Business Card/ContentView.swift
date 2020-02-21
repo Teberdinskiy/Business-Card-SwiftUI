7e0ac4601defac196116ce5ea933c5b1b1f392e7
Alikhan Batchaev <loko-han@mail.ru>
2020-02-21 20:13:02 +0300

    √ Custom font added.

--- Business Card/ContentView.swift
+++ Business Card/ContentView.swift
@@ -10,7 +10,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        ZStack {
+            Color(red: 0.09, green: 0.63, blue: 0.52)
+                .edgesIgnoringSafeArea(.all)
+            Text("Alikhan Batchaev")
+                .font(Font.custom("Pacifico-Regular", size: 40))
+                .bold()
+                .foregroundColor(.white)
+                .multilineTextAlignment(.center)
+        }
     }
 }
 
